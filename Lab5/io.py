9d18e5d8f5049feb22ca0144e88337b489703c6a
ASUS-create <valentina.dobreva.bg@gmail.com>
2025-06-17 12:35:37 +0300

    add HW for Lab 4

--- Lab5/io.py
+++ Lab5/io.py
@@ -1,1 +1,26 @@
-print ('This is the io.py module.')
+#user_name=input('Enter your name:')
+#print (f'user_name: {user_name}')
+
+#x=input('x= ')
+#print (type(x))
+#print(f'x*3= {int(x)*3}')
+
+
+#print ('x')
+
+try:
+    x=int(input('x= '))
+    print (f'x= {x}')
+    print (3/x)
+except ValueError:
+    print ('x must be a number')
+except ZeroDivisionError:
+    print ('x cannot be zero') 
+except:
+    print ('some error occurred')
+
+
+
+т
+
+
